8c661625ea9160782727968c67136d376b9e4147
MykytaHefsymanskyi98 <mykytahefsymanskyi@gmail.com>
2021-09-05 16:50:40 +0300

    Created Main Menu and Game Over Scenes

---
+++ Assets/Scripts/Sceneloader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sceneloader : MonoBehaviour
+{
+   public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void LoadCoreGameScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
